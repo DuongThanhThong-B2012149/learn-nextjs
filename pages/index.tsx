7afb9bdf6af9f47f6711f7a7bc2bdd3f68d5b2a4
thanhthong <thongduong61134@gmail.com>
2022-08-25 21:51:43 +0700

    video-45

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { Seo } from '@/components/commons/seo'
 import { FeatureWork, Hero, RecentPost } from '@/components/home'
 import { MainLayout } from '@/components/layout'
 import { Box } from '@mui/material'
@@ -7,6 +8,14 @@ import styles from '../styles/Home.module.css'
 const Home: NextPageWithLayout = () => {
   return (
     <Box className={styles.container}>
+      <Seo
+        data={{
+          title: 'NextJs tutorials | Herocodedev',
+          description: 'Learn NextJs step by step build website for beginers',
+          url: 'https://learn-nextjs-eight-phi.vercel.app/',
+          thumbnailUrl: 'https://learn-nextjs-eight-phi.vercel.app/',
+        }}
+      />
       <Hero />
       <RecentPost />
       <FeatureWork />
